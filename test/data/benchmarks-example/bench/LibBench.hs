20257ff049692313378f9919a67992cc0a7e9d2b
capitanbatata <damian.nadales@gmail.com>
2017-11-08 15:34:31 +0100

    Added a benchmark program.

--- test/data/benchmarks-example/bench/LibBench.hs
+++ test/data/benchmarks-example/bench/LibBench.hs
@@ -1,4 +1,9 @@
 import           Criterion.Main
+import           Lib
 
 main :: IO ()
-main = undefined
+main = defaultMain
+    [ bench "100 numbers" $ nfIO $ saveNToFile 100 "bench100.txt"
+    , bench "1000 numbers" $ nfIO $ saveNToFile 1000 "bench1000.txt"
+    , bench "10000 numbers" $ nfIO $ saveNToFile 10000 "bench10000.txt"
+    ]
