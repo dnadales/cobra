6ec5969ba682159bbf6255a574f88d734305a547
capitanbatata <damian.nadales@gmail.com>
2017-11-08 16:19:36 +0100

    Benchmarks are now output in a JSON format in the system temporary folder.

--- test/data/benchmarks-example/bench/LibBench.hs
+++ test/data/benchmarks-example/bench/LibBench.hs
@@ -1,9 +1,16 @@
 import           Criterion.Main
+import           Criterion.Types
+import           System.IO.Temp
+
 import           Lib
 
 main :: IO ()
-main = defaultMain
-    [ bench "100 numbers" $ nfIO $ saveNToFile 100 "bench100.txt"
-    , bench "1000 numbers" $ nfIO $ saveNToFile 1000 "bench1000.txt"
-    , bench "10000 numbers" $ nfIO $ saveNToFile 10000 "bench10000.txt"
-    ]
+main = do
+    fp <- emptySystemTempFile "benchmark-results.json"
+    defaultMainWith (config fp)
+        [ bench "100 numbers" $ nfIO $ saveNToFile 100 "bench100.txt"
+        , bench "1000 numbers" $ nfIO $ saveNToFile 1000 "bench1000.txt"
+        , bench "10000 numbers" $ nfIO $ saveNToFile 10000 "bench10000.txt"
+        ]
+    where
+      config fp = defaultConfig { jsonFile = Just fp }
