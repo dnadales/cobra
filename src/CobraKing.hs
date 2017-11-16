583a4ef8b7b92a81bc45b1613faad2b595564956
capitanbatata <damian.nadales@gmail.com>
2017-11-16 17:55:02 +0100

    Preliminary work done with Kerem.

---
+++ src/CobraKing.hs
@@ -0,0 +1,54 @@
+-- | Central module that takes care of running benchmarks, and provides access
+-- to the historical benchmark data.
+
+module CobraKing
+    ()
+where
+
+
+
+-- | Start the benchmarking process.
+triggerBenchmark :: String -> IO ()
+triggerBenchmark = undefined
+--     Setup ->
+
+data SetupData = GithubRepo URL CommitHash BuildCommand
+               | Package InstallCommand
+               | Local BuildCommand
+
+setup :: SetupData -> IO ()
+setup = undefined
+
+runBenchmark :: Command -> IO BenchmarkData
+runBenchmark = undefined
+
+-- Storage module
+store :: BenchmarkData -> IO ()
+store = undefined
+
+fromStore :: Constraints -> IO [DataPoint]
+fromStore =undefined
+
+-------------
+
+doBenchmark :: SetupData -> Command -> Config -> IO ()
+doBenchmark dat cmd cfg = do
+    setup dat
+    bmData <- runBenchmark cmd
+    let bmDataPoints = generateDataPoints (context dat) bmData
+    store bmDataPoints
+    report <- generateReport bmDataPoints
+    alert report (alertTargets cfg)
+
+fetchData :: Constraints -> IO [DataPoint]
+fetchData consts = do
+    fromStore consts
+
+fetchReport :: Constraints -> IO Report
+fetchReport consts = do
+    bmDataPoints <- fromStore consts
+    generateReport bmDataPoints
+
+-- generateDataPoints :: [BenchmarkData] -> Context ->
+
+
